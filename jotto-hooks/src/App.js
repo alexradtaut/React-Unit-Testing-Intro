1c268a421f515880e7cecf995655334f209b6b5c
Alex Taut <alex.taut@SVROLP01894.ro.softvision.global>
2021-01-21 14:05:39 +0200

    UseEffect/UseReducer test

--- jotto-hooks/src/App.js
+++ jotto-hooks/src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import GuessedWords from './components/GuessedWords';
 import Congrats from './components/Congrats';
+import hookActions from './actions/hookActions';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'setSecretWord':
+      return { ...state, secretWord: action.payload };
+    default:
+      throw new Error('Invalid action type');
+  }
+}
 
 function App() {
+  const [state, dispatch] = React.useReducer(reducer, { secretWord: null });
+  const setSecretWord = (secretWord) =>
+    dispatch({ type: 'setSecretWord', payload: secretWord });
+
+  React.useEffect(() => {
+    hookActions.getSecretWord(setSecretWord);
+  }, []);
   return (
     <div className="container" data-test="component-app">
       <h1>Jotto</h1>
--- jotto-hooks/src/App.test.js
+++ jotto-hooks/src/App.test.js
@@ -1,13 +1,25 @@
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import React from 'react';
 import { findByTestAttr } from './tests/testUtils';
 import App from './App';
+import hookActions from './actions/hookActions';
+
+const mockGetSecretWord = jest.fn();
 
 const setup = () => {
-  return shallow(<App />);
+  mockGetSecretWord.mockClear();
+  hookActions.getSecretWord = mockGetSecretWord;
+  return mount(<App />);
 };
 test('App renders without error', () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, 'component-app');
   expect(component.length).toBe(1);
+});
+
+describe('getSecretWord calls', () => {
+  test('getSecretWord gets called on app mount', () => {
+    setup();
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  });
 });
