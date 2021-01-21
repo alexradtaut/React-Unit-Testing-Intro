a39168d70f6faa5026eff4164da0788c4c1911ab
Alex Taut <alex.taut@SVROLP01894.ro.softvision.global>
2021-01-21 10:54:01 +0200

    proptypes testing

--- jotto-hooks/src/tests/unit/Input.test.js
+++ jotto-hooks/src/tests/unit/Input.test.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import Input from '../../components/Input';
-import { findByTestAttr } from '../testUtils';
+import { findByTestAttr, checkProps } from '../testUtils';
 
-const setup = () => {
-  return shallow(<Input />);
+const setup = (secretWord = 'party') => {
+  return shallow(<Input secretWord={secretWord} />);
 };
 
 test('Input renders without errors', () => {
   const wrapper = setup();
   const input = findByTestAttr(wrapper, 'component-input');
   expect(input.length).toBe(1);
+});
+
+test('does not throw warning with expected props', () => {
+  checkProps(Input, { secretWord: 'party' });
 });
