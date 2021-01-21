9443b6e044b53d41d8d3068f57deb748e1210608
Alex Taut <alex.taut@SVROLP01894.ro.softvision.global>
2021-01-21 11:12:50 +0200

    Input on change test

--- jotto-hooks/src/components/Input.js
+++ jotto-hooks/src/components/Input.js
@@ -2,7 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Input({ secretWord }) {
-  return <div data-test="component-input"></div>;
+  const [currentGuess, setCurrentGuess] = React.useState('');
+  return (
+    <div data-test="component-input">
+      <form className="form-inline">
+        <input
+          type="text"
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          placeholder="enter guess"
+          value={currentGuess}
+          onChange={(e) => setCurrentGuess(e.target.value)}
+        />
+        <button
+          data-test="submit-button"
+          className="btn btn-primary mb-2"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 Input.propTypes = {
